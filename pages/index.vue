a7f592a4e9c433407351f3e12b3a60ef3a862edf
guancong <guancong@elensdata.com>
2019-07-18 14:37:39 +0800

    layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="home">
-     <el-link href="/canvas" target="_blank">canvas</el-link>
-     <el-link href="/svg" target="_blank">svg</el-link>
-     <el-link href="/demo" target="_blank">svg</el-link>
-
-  </div>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-link href="/canvas" target="_blank">canvas</el-link>
+        <el-link href="/svg" target="_blank">svg</el-link>
+        <el-link href="/demo" target="_blank">svg</el-link>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+    <el-footer>Footer</el-footer>
+  </el-container>
 </template>
